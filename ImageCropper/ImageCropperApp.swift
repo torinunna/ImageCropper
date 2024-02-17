71572af670e11825c1fa1a54669c5d94de9e16d0
torinunna <torinunna9588@gmail.com>
2024-02-17 20:11:50 +0900

    Initial Commit

---
+++ ImageCropper/ImageCropperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCropperApp.swift
+//  ImageCropper
+//
+//  Created by YUJIN KWON on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageCropperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
